27a22987e95c956afb41e62fe5fba9e2b3e4c7b1
namankr301 <namankr301@gmail.com>
2021-08-01 08:03:05 +0530

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+from app.config import Config
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+ 
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
+
+mail = Mail(app)
+
+
+from app.users.routes import  users
+from app.posts.routes import  posts
+from app.main.routes import  main
+
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
